c40b511981d742c3d63432ba35c681981b825c75
Jawdat Abadullah <Jawdat.89@outlook.com>
2022-10-12 21:58:02 +0300

    bad way to handle a form

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -4,6 +4,7 @@ const SimpleInput = (props) => {
   const nameInputRef = useRef();
 
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -13,8 +14,10 @@ const SimpleInput = (props) => {
     event.preventDefault(); // to disable the default behaviour of the form tag
 
     if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
       return;
     }
+    setEnteredNameIsValid(true);
 
     console.log(enteredName);
 
@@ -22,9 +25,11 @@ const SimpleInput = (props) => {
     setEnteredName('');
   };
 
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -33,6 +38,7 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
+        {!enteredNameIsValid && <p className="errorText">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
