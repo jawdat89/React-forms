4d2b115e659fb1f2edba9ebd6282041f0d0e9150
Jawdat Abadullah <Jawdat.89@outlook.com>
2022-10-12 21:27:27 +0300

    using useRef

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,6 +2,8 @@ import React, {useRef, useState} from 'react';
 
 const SimpleInput = (props) => {
 
+  const nameInputRef = useRef();
+
   const [enteredName, setEnteredName] = useState('');
 
   const nameInputChangeHandler = event => {
@@ -11,14 +13,17 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = event => {
     event.preventDefault(); // to disable the default behaviour of the form tag
 
-    console.log(enteredName)
+    
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue)
+
   }
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputChangeHandler}/>
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
