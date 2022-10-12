bddc6b35d9bcf933e5d4fb82fccddda3de251fa8
Jawdat Abadullah <Jawdat.89@outlook.com>
2022-10-12 21:43:22 +0300

    se form value

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,29 +1,34 @@
-import React, {useRef, useState} from 'react';
+import React, { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
-
   const nameInputRef = useRef();
 
-  const [enteredName, setEnteredName] = useState('');
+  const [enteredName, setEnteredName] = useState("");
 
-  const nameInputChangeHandler = event => {
+  const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
-  const formSubmissionHandler = event => {
+  const formSubmissionHandler = (event) => {
     event.preventDefault(); // to disable the default behaviour of the form tag
 
-    
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue)
+    console.log(enteredName);
 
-  }
+    // nameInputRef.current.value = ''; // Not IDEAL. DO NOT MANIPULATE THE DOM
+    setEnteredName('');
+  };
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler}/>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
